3216107b1cc64d6e5ae80510444a723cbb554807
Nguyễn Hạnh <huuhanh010602@gmail.com>
2023-04-21 18:46:16 +0700

    Fix ESC

--- Test_boardC21/Src/motor.c
+++ Test_boardC21/Src/motor.c
@@ -9,6 +9,22 @@ void calculate_motor_output(uint16_t *esc_right, uint16_t *esc_left, uint16_t *s
     // value PWM
     *esc_right = throttle_rc + pid->PID_roll_out;
     *esc_left = throttle_rc - pid->PID_roll_out;
+    if(*esc_right < 1000)
+    {
+    	*esc_right = 1000;
+    }
+    if (*esc_right > 2000)
+    {
+    	*esc_right = 2000;
+    }
+    if(*esc_left < 1000)
+    {
+        *esc_left = 1000;
+    }
+    if (*esc_left > 2000)
+    {
+        *esc_left = 2000;
+    }
     *servo_right = 1500 + pid->PID_pitch_out - pid->PID_yaw_out + SERVO_RIGHT_OFFSET;
     *servo_left = 1500 - pid->PID_pitch_out - pid->PID_yaw_out + SERVO_LEFT_OFFSET;
 }
