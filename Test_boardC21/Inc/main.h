adda67546996842d9f8c27711b66aa7aa4f8dd05
nga <nga.leddt02@hcmut.edu.vn>
2023-04-27 12:08:25 +0700

    setting

--- Test_boardC21/Inc/main.h
+++ Test_boardC21/Inc/main.h
@@ -24,8 +24,7 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
@@ -38,11 +37,7 @@ extern "C"
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
-<<<<<<< HEAD
 #define DEBUG_MPU	1
-=======
-#define DEBUG_MPU 1
->>>>>>> parent of d4df473 (optimizing PID)
 #define DEBUG_PWM 0
 #define DEBUG_RX 0
 #define TUNING 1
@@ -63,22 +58,22 @@ extern "C"
 #define Kd_yaw 3.0
 #endif
 
-    /* USER CODE END ET */
+/* USER CODE END ET */
 
-    /* Exported constants --------------------------------------------------------*/
-    /* USER CODE BEGIN EC */
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
 
-    /* USER CODE END EC */
+/* USER CODE END EC */
 
-    /* Exported macro ------------------------------------------------------------*/
-    /* USER CODE BEGIN EM */
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
 
-    /* USER CODE END EM */
+/* USER CODE END EM */
 
-    void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 
-    /* Exported functions prototypes ---------------------------------------------*/
-    void Error_Handler(void);
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
@@ -107,9 +102,9 @@ extern "C"
 #define NRF_MOSI_GPIO_Port GPIOB
 #define NRF_CE_Pin GPIO_PIN_8
 #define NRF_CE_GPIO_Port GPIOA
-    /* USER CODE BEGIN Private defines */
+/* USER CODE BEGIN Private defines */
 
-    /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
